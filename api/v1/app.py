95489ed7210b8510644c4499e8c9e15b2c01a641
Eric20Junior <eric20junior@gmail.com>
2022-04-18 21:07:51 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,8 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 '''
     app for registering blueprint and starting flask
 '''
-from flask import Flask, make_respone, jsonify
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
@@ -16,8 +16,11 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def tear_down(self):
-    """close query after session.."""
+    '''
+    close query after each session
+    '''
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -25,6 +28,7 @@ def not_found(error):
     return JSON formatted 404 status code response
     '''
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
--- models/user.py
+++ models/user.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 import hashlib
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-        __tablename__ = 'users'
-        if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -40,7 +42,3 @@ class User(BaseModel, Base):
             fset the password with hash
         '''
         self._password = hashlib.md5(value.encode('utf8')).hexdigest()
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
